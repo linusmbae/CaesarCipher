ff3075ea22a9376a762e53db5dff14e20e97e1fe
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-18 20:43:57 +0300

    first test pass

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,3 +1,10 @@
 public class CaesarCipher {
+    private String encrypt;
+    private int jump;
+
+    public CaesarCipher(String encrypt, int jump) {
+
+    }
+
 
 }
--- src/test/java/CaesarCipherEncryptTest.java
+++ src/test/java/CaesarCipherEncryptTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 public class CaesarCipherEncryptTest {
     @Test
     public void encryptData_whenEncrypted_true() {
-
+        CaesarCipher testEncryption=new CaesarCipher("encrypt", 4);
+        assertEquals(true,testEncryption instanceof CaesarCipher);
     }
 }
