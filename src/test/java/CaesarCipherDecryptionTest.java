57ebdbddd54b7e34ef04e4a1d278400d40ce297f
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-18 21:10:00 +0300

    decryption instace passed

--- src/test/java/CaesarCipherDecryptionTest.java
+++ src/test/java/CaesarCipherDecryptionTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 public class CaesarCipherDecryptionTest {
     @Test
     public void decryptData_instantiatesCorrectly_true() {
-
+        CaesarCipher testCaesarCipher = new CaesarCipher("irgvctx",4);
+        assertEquals(true, testCaesarCipher instanceof CaesarCipher);
     }
 }
